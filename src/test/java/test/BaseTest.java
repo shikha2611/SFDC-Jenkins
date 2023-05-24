050188cb61cca7e6434a179c3431284f2cb61027
shikha2611 <shikha2611@gmail.com>
2023-05-24 17:19:04 -0600

    added Parameter SystemPropertyVariable tag in POM.xml and will pass paramater value thru Jenkins

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -12,6 +12,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
+
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
@@ -26,6 +28,7 @@ public class BaseTest {
 	public static ExtentReports extent = null;
 
 	protected static Logger log = LogManager.getLogger(BaseTest.class.getName());
+
 //
 //	protected static WebDriver driver;
 ////	cross browser testing
@@ -36,11 +39,11 @@ public class BaseTest {
 //		threadLocalDriver.set(driver);
 //	}
 //	
-
+	@Parameters("Browser")
 	@BeforeMethod
-	public void setDriver() throws IOException {
+	public void setDriver(String localbrowser) throws IOException {
 		log.info("setDriver()......Initialization");
-		WebDriver driver = BaseTest.getBrowserDriver(PropertyReader.readbrowserType("browser"), false);
+		WebDriver driver = BaseTest.getBrowserDriver(localbrowser);
 		threadLocalDriver.set(driver); // sets the local copy for the browser driver.
 		log.info("setDriver()......Successfully done");
 	}
@@ -69,44 +72,32 @@ public class BaseTest {
 	// to add the report in the html format
 	public static void extentReport() {
 		extent = new ExtentReports();
-		ExtentSparkReporter spark = new ExtentSparkReporter("target/"+"Spark"+System.currentTimeMillis()+".html");
+		ExtentSparkReporter spark = new ExtentSparkReporter("target/" + "Spark" + System.currentTimeMillis() + ".html");
 		spark.config().setTheme(Theme.DARK);
 		spark.config().setDocumentTitle("MyReport");
 		extent.attachReporter(spark);
 	}
 
-	public static WebDriver getBrowserDriver(String browsername, boolean headless) {
+	public static WebDriver getBrowserDriver(String browsername) {
 
 		String browser = browsername.toLowerCase();
-		WebDriver driver=null;
+		WebDriver driver = null;
 		switch (browser) {
-		
 		case "chrome":
 			WebDriverManager.chromedriver().setup();
-			if (headless) {
-				ChromeOptions co = new ChromeOptions();
-//				co.addArguments("--headless");
-				 driver = new ChromeDriver(co);
-			} else {
-				driver = new ChromeDriver();
-			}
+			driver = new ChromeDriver();
 			break;
 
 		case "edge":
 			WebDriverManager.edgedriver().setup();
-			if (headless) {
-				EdgeOptions eo = new EdgeOptions();
-//				eo.addArguments("--headless");
-				driver = new EdgeDriver(eo);
-			} else {
-				driver = new EdgeDriver();
-			}
+			driver = new EdgeDriver();
 			break;
 
 		default:
 			System.out.println("Please use either Chrome or edge broswer");
 
 		}
+		driver.manage().window().maximize();
 		return driver;
 
 	}
